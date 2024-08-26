8fe61d24ab23571a843b1049d04485a917b96997
mchiv-hpe <max.chivers@hpe.com>
2024-08-26 10:05:12 +0100

    important updates to code

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func fibonacci(n int) int {
+
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
 func main() {
 	fmt.Println("Hello World")
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(fibonacci(i))
+	}
 }
